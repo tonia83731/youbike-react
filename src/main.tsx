0e6204e9b7cd6a3810a1a53463a468ce58d909e2
tonia83731 <huiyu.tonia.chang@gmail.com>
2025-07-25 09:54:37 +0800

    feat: clean up

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 import 'react-leaflet-markercluster/styles'
-// import 'react-leaflet-markercluster/dist/styles.min.css';
+
 import App from './App.tsx'
 import FilterProvider from './context/filterContext.tsx'
 
